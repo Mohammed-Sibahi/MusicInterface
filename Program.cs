fe60764c7f17bff16f1e2ca5bd76863bef277240
Sibahi <sibahi@live.com>
2023-11-21 15:38:22 +0400

    use switch method to choose what to do

--- Program.cs
+++ Program.cs
@@ -34,3 +34,62 @@ public class SimpleMusicPlayer : IMusicPlayer
         Console.WriteLine("Play previous song!");
     }
 }
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IMusicPlayer player = new SimpleMusicPlayer();
+        
+        while (true)
+        {
+            Console.WriteLine("Select an option: ");
+            Console.WriteLine("1. play");
+            Console.WriteLine("2. pause");
+            Console.WriteLine("3. stop");
+            Console.WriteLine("4. next");
+            Console.WriteLine("5. previous");
+            Console.WriteLine();
+            Console.WriteLine("Press (6) to exit.");
+
+            if(int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                case 1:
+                        player.Play();
+                        break;
+                case 2: 
+                        player.Pause(); 
+                        break;
+
+                case 3: 
+                        player.Stop(); 
+                        break;
+
+                case 4: 
+                        player.Next(); 
+                        break;
+
+                case 5: 
+                        player.Previous(); 
+                        break;
+                case 6:
+                Console.WriteLine("Exiting the program.");
+                        return;
+                default: 
+                        Console.WriteLine("Invalid option. Please select a valid option.");
+                        break;
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
