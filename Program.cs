380f4001f97a81ebaffb95f578cfa32b0747f6b6
Sibahi <sibahi@live.com>
2023-11-21 15:08:53 +0400

    create IMusicPlayer interface

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System; 
+
+public interface IMusicPlayer
+{
+    public void Play();
+    public void Pause();
+    public void Stop();
+    public void Next();
+    public void Previous();
+
+}
+
